d2ba1b9f5aa7035284066197c8e15f8878b7ebba
Samantha Meyer <thissammeyer@gmail.com>
2023-03-04 18:13:21 -0500

    set up player movement and collision

--- Assets/Scripts/MatchPlayerHeight.cs
+++ Assets/Scripts/MatchPlayerHeight.cs
@@ -11,12 +11,19 @@ public class MatchPlayerHeight : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        capsule = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // calculate the height difference between main camera and floor
+        float headHeight = mainCamera.position.y - floorReference.position.y;
+
+        // move capsule directly between main camera and floor
+        transform.position = mainCamera.position - Vector3.up * headHeight / 2;
+
+        // set capsule height to match head height
+        capsule.height = headHeight;
     }
 }
